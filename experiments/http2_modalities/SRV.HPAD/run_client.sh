d0c01df416a0a75cf46e5a477871f720e9cff448
Bogdan Cebere <bogdan.cebere@cispa.de>
2025-01-14 15:16:08 +0100

    more experiments

---
+++ experiments/http2_modalities/SRV.HPAD/run_client.sh
@@ -0,0 +1,1 @@
+sudo python client.py --dst_ip $1 --dst_port $2 --ifname $3
---
+++ experiments/http2_modalities/SRV.HPAD/run_server.sh
@@ -0,0 +1,5 @@
+python ./server.py --dst_port $1 \
+    --use_server_push 0 \
+    --use_rnd_multiplexing 0 \
+    --use_rnd_hpack 1 \
+    --use_hints103 0
