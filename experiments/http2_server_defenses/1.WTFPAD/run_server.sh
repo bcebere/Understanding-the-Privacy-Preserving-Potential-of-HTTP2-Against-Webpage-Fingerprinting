62c9fad5595e81267e15ff595faa2fcacf1cd533
Bogdan Cebere <bogdan.cebere@cispa.de>
2025-01-14 17:07:32 +0100

    add server defenses

---
+++ experiments/http2_server_defenses/1.WTFPAD/run_server.sh
@@ -0,0 +1,7 @@
+python ./server.py --dst_port $1 \
+    --defense_batch 0 \
+    --defense_inject 0 \
+    --defense_delay_random 1 \
+    --defense_delay_constant 0 \
+    --defense_pad_random 1 \
+    --defense_pad_constant 0
---
+++ experiments/http2_server_defenses/2.TMORPH/example/run_client.sh
@@ -0,0 +1,1 @@
+../run_client.sh
